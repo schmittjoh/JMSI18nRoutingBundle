9c5b7ae17c31cbf93b37c7d92bf10c9fc3771437
Jordi Boggiano <j.boggiano@seld.be>
2011-10-27 11:50:07 +0300

    Fix typo

--- Router/I18nRouter.php
+++ Router/I18nRouter.php
@@ -98,7 +98,7 @@ class I18nRouter extends Router
         // skip internal routes
         if ('_' !== $name[0]) {
             // if an absolute URL is requested, we set the correct host
-            if ($this->hostMap) {
+            if ($absolute) {
                 $currentHost = $this->context->getHost();
                 $this->context->setHost($this->hostMap[$locale]);
             }
@@ -126,7 +126,7 @@ class I18nRouter extends Router
 
     /**
      * Tries to match a URL with a set of routes.
-	 *
+     *
      * Returns false if no route matches the URL.
      *
      * @param  string $url URL to be parsed
