2dd8358cb2b90bc967354d23bfd4d5ef041d19cf
Christian Flach <cmfcmf.flach@gmail.com>
2015-02-09 17:34:02 +0100

    Do not try to "brute-force"-translate routes but check if the translation catalogue includes a translation.

--- Router/DefaultPatternGenerationStrategy.php
+++ Router/DefaultPatternGenerationStrategy.php
@@ -3,7 +3,9 @@
 namespace JMS\I18nRoutingBundle\Router;
 
 use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Translation\LoggingTranslator;
 use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Component\Translation\TranslatorBagInterface;
 use Symfony\Component\Routing\Route;
 
 /**
@@ -42,14 +44,28 @@ class DefaultPatternGenerationStrategy implements PatternGenerationStrategyInter
     {
         $patterns = array();
         foreach ($route->getOption('i18n_locales') ?: $this->locales as $locale) {
-            // if no translation exists, we use the current pattern
-            if ($routeName === $i18nPattern = $this->translator->trans($routeName, array(), $this->translationDomain, $locale)) {
-                $i18nPattern = $route->getPattern();
+            // Check if translation exists in the translation catalogue to avoid errors being logged by 
+            // the new LoggingTranslator of Symfony 2.6. However, the LoggingTranslator did not implement
+            // the interface until Symfony 2.6.5, so an extra check is needed.
+            if ($this->translator instanceof TranslatorBagInterface || $this->translator instanceof LoggingTranslator) {
+                // Check if route is translated.
+                if (!$this->translator->getCatalogue($locale)->has($routeName, $this->translationDomain)) {
+                    // No translation found.
+                    $i18nPattern = $route->getPattern();
+                } else {
+                    // Get translation.
+                    $i18nPattern = $this->translator->trans($routeName, array(), $this->translationDomain, $locale);
+                }
+            } else {
+                // if no translation exists, we use the current pattern
+                if ($routeName === $i18nPattern = $this->translator->trans($routeName, array(), $this->translationDomain, $locale)) {
+                    $i18nPattern = $route->getPattern();
+                }
             }
 
             // prefix with locale if requested
             if (self::STRATEGY_PREFIX === $this->strategy
-                    || (self::STRATEGY_PREFIX_EXCEPT_DEFAULT === $this->strategy && $this->defaultLocale !== $locale)) {
+                || (self::STRATEGY_PREFIX_EXCEPT_DEFAULT === $this->strategy && $this->defaultLocale !== $locale)) {
                 $i18nPattern = '/'.$locale.$i18nPattern;
                 if (null !== $route->getOption('i18n_prefix')) {
                     $i18nPattern = $route->getOption('i18n_prefix').$i18nPattern;
