9f95f50569d6f91cbe8ac99edc78b2e2d26b9d97
Łukasz Cieślak <lukasz.cieslak2@komputronik.pl>
2020-03-31 21:38:12 +0200

    Repair Translation namespace

--- Router/DefaultPatternGenerationStrategy.php
+++ Router/DefaultPatternGenerationStrategy.php
@@ -4,7 +4,7 @@ namespace JMS\I18nRoutingBundle\Router;
 
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Translation\LoggingTranslator;
-use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Component\Translation\TranslatorBagInterface;
 use Symfony\Component\Routing\Route;
 
